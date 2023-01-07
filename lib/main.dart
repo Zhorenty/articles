659b8b756806d2a97691354c3757dd6e7157a390
George <zhorenty@gmail.com>
2023-01-07 13:52:30 +0300

    freezed + serializable

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:new_udemy_course/pages/web_local_page.dart';
+import 'package:new_udemy_course/models/user_freezed.dart';
+import 'package:new_udemy_course/models/location.dart';
 
 void main() {
-  runApp(const MyApp());
-}
+  User userA = const User(
+      name: 'User', age: 30, locations: [Location(zipcode: '4432342')]);
+  print(userA.toJson());
+  // User userB = const User(name: 'User', age: 30);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // log(userA.hashCode.toString());
+  // log(userB.hashCode.toString());
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: const WebViewLocalPage(),
-    );
-  }
+  // print(userA == userB);
+
+  // // copyWith
+  // final userCopyA = userA.copyWith(
+  //   name: 'User A',
+  // );
+
+  // print('User A Copy: $userCopyA');
+  // //serialization
+  // final json = userA.toJson();
+  // print('User A toJson: $json');
+  // print('User A fromJson: ${User.fromJson(json) ');
 }
