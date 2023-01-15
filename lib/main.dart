395d0ef949d8f4b9857a3b6055572de2e34478dc
George <zhorenty@gmail.com>
2023-01-15 14:15:38 +0300

    unique_key

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:new_udemy_course/bloc_observable.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:new_udemy_course/ui/pages/home_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized(); //соединяет фреймворк и движок flutter
-
-  final storage = await HydratedStorage.build(
-    storageDirectory: await getTemporaryDirectory(),
-  );
-
-  HydratedBlocOverrides.runZoned(
-    () => runApp(const MyApp()),
-    blocObserver: CharacterBlocObservable(),
-    // storage: storage,
-  );
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -25,25 +12,101 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Rick and Morty',
-      theme: ThemeData(
-          brightness: Brightness.light,
-          primaryColor: Colors.black,
-          fontFamily: 'Georgia',
-          textTheme: const TextTheme(
-            headline1: TextStyle(
-                fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white),
-            headline2: TextStyle(
-                fontSize: 30, fontWeight: FontWeight.w700, color: Colors.white),
-            headline3: TextStyle(fontSize: 24, color: Colors.white),
-            bodyText1: TextStyle(
-                fontSize: 16, fontWeight: FontWeight.w500, color: Colors.white),
-            bodyText2: TextStyle(
-                fontSize: 12, fontWeight: FontWeight.w200, color: Colors.white),
-            caption: TextStyle(
-                fontSize: 11, fontWeight: FontWeight.w100, color: Colors.grey),
-          )),
-      home: HomePage(title: 'Rick and Morty'),
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late List<Widget> tiles;
+
+  @override
+  void initState() {
+    super.initState();
+
+    tiles = [
+      Padding(
+        key: UniqueKey(),
+        padding: const EdgeInsets.all(8.0),
+        child: const ColorWidget(text: '1'),
+      ),
+      Padding(
+        key: UniqueKey(),
+        padding: const EdgeInsets.all(8.0),
+        child: const ColorWidget(text: '2'),
+      ),
+    ];
+  }
+
+  void swapTiles() {
+    setState(() {
+      tiles.insert(1, tiles.removeAt(0));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UniqueKey'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: tiles,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: swapTiles,
+        child: const Icon(Icons.swap_horiz, size: 32),
+      ),
+    );
+  }
+}
+
+class ColorWidget extends StatefulWidget {
+  final String text;
+
+  const ColorWidget({Key? key, required this.text}) : super(key: key);
+
+  @override
+  State<ColorWidget> createState() => _ColorWidgetState();
+}
+
+class _ColorWidgetState extends State<ColorWidget> {
+  final Color myColor = GenerateColor.getColor();
+
+  @override
+  Widget build(BuildContext context) {
+    print(
+        '$myColor, element ${context.hashCode} render: ${context.findRenderObject().hashCode}');
+    return Container(
+      height: 150,
+      width: 150,
+      color: myColor,
+      child: Center(child: Text(widget.text)),
+    );
+  }
+}
+
+class GenerateColor {
+  static final random = Random();
+  static Color getColor() {
+    return Color.fromARGB(
+      255,
+      random.nextInt(255),
+      random.nextInt(255),
+      random.nextInt(255),
     );
   }
 }
